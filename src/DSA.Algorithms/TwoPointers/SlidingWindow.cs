2a1228b0788e71e056a8f949cbd1b14f71c5ff1c
Jesus <jesus.sr0704@gmail.com>
2026-09-02 20:38:09 -0600

    Added WindowsSlinding problem and tests

--- src/DSA.Algorithms/TwoPointers/SlidingWindow.cs
+++ src/DSA.Algorithms/TwoPointers/SlidingWindow.cs
@@ -2,5 +2,28 @@ namespace DSA.Algorithms.TwoPointers;
 
 public class SlidingWindow
 {
+ 
+    public static int LongestSubstringLength(string str)
+    {
+        // "abbcasa"
+        int left = 0;
+        int right = 0;
+        var hashTable = new Dictionary<char, int>();
+        int longestLength = 0;
     
+        while (right < str.Length)
+        {
+            if (hashTable.ContainsKey(str[right]) && hashTable[str[right]] >= left )
+            {
+                left = hashTable[str[right]] + 1;
+            }
+        
+            longestLength = (longestLength > (right - left) + 1) ? longestLength : (right - left) + 1;
+            hashTable[str[right]] = right;
+        
+            right++;
+        }
+
+        return longestLength;
+    }
 }
--- tests/DSA.Tests/Algorithms/TwoPointers/SlidingWindowTest.cs
+++ tests/DSA.Tests/Algorithms/TwoPointers/SlidingWindowTest.cs
@@ -1,6 +1,59 @@
+using DSA.Algorithms.TwoPointers;
+
 namespace DSA.Tests.Algorithms.TwoPointers;
+
 
 public class SlidingWindowTest
 {
+    [Fact]
+    public void LongestSubstringLength_EmptyString_ReturnsZero()
+    {
+        // Arrange
+        string str = string.Empty;
+        
+        // Act
+        var result = SlidingWindow.LongestSubstringLength(str);
+        
+        // Assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void LongestSubstringLength_String_ReturnsLength()
+    {
+        // Arrange
+        string str = "Hello World";
+        
+        // Act
+        var result = SlidingWindow.LongestSubstringLength(str);
+        
+        // Assert
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void LongestSubstringLength_SameString_ReturnsOne()
+    {
+        // Arrange
+        string str = "aaaaaaaaaa";
+        
+        // Act
+        var result = SlidingWindow.LongestSubstringLength(str);
+        
+        // Assert
+        Assert.Equal(1, result);
+    }
     
+    [Fact]
+    public void LongestSubstringLength_NonRepetingString_ReturnsLength()
+    {
+        // Arrange
+        string str = "asdfghjklñ";
+        
+        // Act
+        var result = SlidingWindow.LongestSubstringLength(str);
+        
+        // Assert
+        Assert.Equal(str.Length, result);
+    }
 }
